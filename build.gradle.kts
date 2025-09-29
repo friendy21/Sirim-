4879e3a09b3c14ef480c58c36c980e559c1c82e5
friendy <friendykaliman@gmail.com>
2025-09-29 14:02:32 +0800

    Delete build.gradle.kts

--- build.gradle.kts
+++
@@ -1,7 +0,0 @@
-plugins {
-    id("com.android.application") apply false
-    id("com.android.library") apply false
-    id("org.jetbrains.kotlin.android") apply false
-    id("org.jetbrains.kotlin.jvm") apply false
-    id("com.google.dagger.hilt.android") apply false
-}
